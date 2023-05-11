b60427ecbb7aa91825017d9d46b30f05ed611cf6
samoel <samuellopescosta@gmail.com>
2023-05-11 10:44:38 -0300

    pegando conceitos

---
+++ routes/tenant.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+use App\Http\Controllers\Tenant\Site\Homecontroller;
+use App\Http\Controllers\Tenant\Admin\HomeAdminController;
+use App\Http\Controllers\Tenant\Admin\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Tenant Routes
+|--------------------------------------------------------------------------
+|
+| Here you can register the tenant routes for your application.
+| These routes are loaded by the TenantRouteServiceProvider.
+|
+| Feel free to customize them however you want. Good luck!
+|
+*/
+
+Route::middleware([
+    'web',
+    InitializeTenancyByDomain::class,
+    PreventAccessFromCentralDomains::class,
+])->group(function () {
+    Route::get('/', [Homecontroller::class , 'index']);
+
+    Route::get('admin/dashboard', [DashboardController::class , 'index']);
+
+
+});
